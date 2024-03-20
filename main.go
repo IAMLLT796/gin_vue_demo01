61abba5c94d574d3c26a56c08ab2b2d9d117ef35
IAMLLT796 <lt.liu796@gmail.com>
2024-03-20 22:24:51 +0800

    项目重构及分离

--- main.go
+++ main.go
@@ -1,111 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"ginEssential/common"
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"log"
-	"math/rand"
-	"net/http"
-	"time"
 )
 
-type User struct {
-	gorm.Model
-	Name      string `gorm:"type:varchar(20);not null"`
-	Telephone string `gorm:"type:varchar(110);not null;unique"`
-	Password  string `gorm:"size:255;not null"`
-}
-
 func main() {
-	db := InitDB()
+	db := common.InitDB()
 	defer db.Close()
 
 	r := gin.Default()
-	r.POST("/api/auth/register", func(ctx *gin.Context) {
-		// 获取参数
-		name := ctx.PostForm("name")
-		telephone := ctx.PostForm("telephone")
-		password := ctx.PostForm("password")
+	r = CollectRoute(r)
 
-		// 数据验证
-		if len(telephone) != 11 {
-			ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为 11 位"})
-			return
-		}
-		if len(password) < 6 {
-			ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于 6 位"})
-			return
-		}
-		// 如果名称没有传, 给一个 10 位的随机字符串
-		if len(name) == 0 {
-			name = RandomString(10)
-		}
-
-		log.Println(name, telephone, password)
-
-		// 判断手机号是否存在
-		if IsTelephoneExist(db, telephone) {
-			ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户已经存在"})
-			return
-		}
-		// 创建用户
-		newUser := User{
-			Name:      name,
-			Telephone: telephone,
-			Password:  password,
-		}
-		db.Create(&newUser)
-		// 返回结果
-		ctx.JSON(200, gin.H{
-			"msg": "注册成功",
-		})
-	})
 	panic(r.Run()) // 监听并在 0.0.0.0:8080 上启动服务
-}
-
-func IsTelephoneExist(db *gorm.DB, telephone string) bool {
-	var user User
-	db.Where("telephone = ?", telephone).First(&user)
-	if user.ID != 0 {
-		return true
-	}
-	return false
-}
-func RandomString(n int) string {
-	var letters = []byte("asdfghjklzxcvbnmqwertyuiopASDFGHJKL")
-
-	result := make([]byte, n)
-	rand.Seed(time.Now().Unix())
-	for i := range result {
-		result[i] = letters[rand.Intn(len(letters))]
-	}
-	return string(result)
-}
-
-func InitDB() *gorm.DB {
-	driverName := "mysql"
-	host := "localhost"
-	port := "3306"
-	database := "ginessential"
-	username := "root"
-	password := "123456"
-	charset := "utf8"
-	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true&loc=Local",
-		username,
-		password,
-		host,
-		port,
-		database,
-		charset,
-	)
-	db, err := gorm.Open(driverName, args)
-	if err != nil {
-		panic("failed to connect database err" + err.Error())
-	}
-
-	db.AutoMigrate(&User{})
-
-	return db
 }
