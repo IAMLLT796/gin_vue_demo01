666200ce9297149a1e8f213a3733551853d3ffff
IAMLLT796 <lt.liu796@gmail.com>
2024-03-20 21:50:52 +0800

    连接数据库实现用户注册

--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"log"
 	"math/rand"
 	"net/http"
 	"time"
 )
 
+type User struct {
+	gorm.Model
+	Name      string `gorm:"type:varchar(20);not null"`
+	Telephone string `gorm:"type:varchar(110);not null;unique"`
+	Password  string `gorm:"size:255;not null"`
+}
+
 func main() {
+	db := InitDB()
+	defer db.Close()
+
 	r := gin.Default()
 	r.POST("/api/auth/register", func(ctx *gin.Context) {
 		// 获取参数
@@ -33,11 +46,18 @@ func main() {
 		log.Println(name, telephone, password)
 
 		// 判断手机号是否存在
-
+		if IsTelephoneExist(db, telephone) {
+			ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户已经存在"})
+			return
+		}
 		// 创建用户
-
+		newUser := User{
+			Name:      name,
+			Telephone: telephone,
+			Password:  password,
+		}
+		db.Create(&newUser)
 		// 返回结果
-
 		ctx.JSON(200, gin.H{
 			"msg": "注册成功",
 		})
@@ -45,6 +65,14 @@ func main() {
 	panic(r.Run()) // 监听并在 0.0.0.0:8080 上启动服务
 }
 
+func IsTelephoneExist(db *gorm.DB, telephone string) bool {
+	var user User
+	db.Where("telephone = ?", telephone).First(&user)
+	if user.ID != 0 {
+		return true
+	}
+	return false
+}
 func RandomString(n int) string {
 	var letters = []byte("asdfghjklzxcvbnmqwertyuiopASDFGHJKL")
 
@@ -54,4 +82,30 @@ func RandomString(n int) string {
 		result[i] = letters[rand.Intn(len(letters))]
 	}
 	return string(result)
+}
+
+func InitDB() *gorm.DB {
+	driverName := "mysql"
+	host := "localhost"
+	port := "3306"
+	database := "ginessential"
+	username := "root"
+	password := "123456"
+	charset := "utf8"
+	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true&loc=Local",
+		username,
+		password,
+		host,
+		port,
+		database,
+		charset,
+	)
+	db, err := gorm.Open(driverName, args)
+	if err != nil {
+		panic("failed to connect database err" + err.Error())
+	}
+
+	db.AutoMigrate(&User{})
+
+	return db
 }
